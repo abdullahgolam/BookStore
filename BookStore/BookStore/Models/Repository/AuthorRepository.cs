f1eb4432c95689c78a1cf801505a1c637b8c12ec
ABDULLAH GOLAM <abdullahagolam@gmail.com>
2020-07-17 23:55:15 +0300

    start from bigining

---
+++ BookStore/BookStore/Models/Repository/AuthorRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Models.Repository
+{
+    public class AuthorRepository : IBookstoreRepository<Author>
+    {
+        IList<Author> authors;
+        public AuthorRepository()
+        {
+            authors = new List<Author>()
+            {
+                new Author
+                {
+                    Id=1, FullName="Abdullah"
+                },
+                new Author
+                {
+                    Id=2, FullName="Marwah"
+                },
+                new Author
+                {
+                    Id=3, FullName="Mohammed"
+                },
+            };
+        }
+        public void Add(Author entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Author Find(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Author> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, Author entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BookStore/BookStore/Models/Repository/BookRepository.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Models.Repository
+{
+    public class BookRepository : IBookstoreRepository<Book>
+    {
+        IList<Book> books;
+
+        public BookRepository()
+        {
+            books = new List<Book>()
+            {
+                new Book
+                {
+                    Id = 1, Title= "C# Programming", Description="No Description"
+                },
+                new Book
+                {
+                    Id = 2, Title= "Java Programming", Description="Nothing"
+                },
+                new Book
+                {
+                    Id = 3, Title= "Python Programming", Description="No Data"
+                },
+            };
+        }
+
+        public void Add(Book entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Book Find(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Book> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, Book entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
