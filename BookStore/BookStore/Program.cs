32b6eff48f0ea1114a70e3ba96065cac6c60078e
ABDULLAH GOLAM <abdullahagolam@gmail.com>
2020-08-03 23:54:46 +0300

    Deploment and publishing

--- BookStore/BookStore/Program.cs
+++ BookStore/BookStore/Program.cs
@@ -2,8 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookStore.Models;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -13,10 +16,23 @@ namespace BookStore
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var webHost = CreateWebHostBuilder(args).Build();
+
+            RunMigrations(webHost);
+
+            webHost.Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        private static void RunMigrations(IHost webHost)
+        {
+            using (var scope = webHost.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<BookstoreDbContext>();
+                db.Database.Migrate();
+            }
+        }
+
+        public static IHostBuilder CreateWebHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
